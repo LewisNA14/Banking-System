0fad11287225f1394c6c80f24c94091ebca4ecfa
LewisNA14 <134816895+LewisNA14@users.noreply.github.com>
2026-01-19 20:36:16 +0000

    Add files via upload
    Main Commit 2:
    
    Tried to do some work for adding and managing users of the banking system, it's not really complete but it's a start.
    
    Similarly tried to do the same but for an interactive menu for a Customer or Admin to use (i'll have to manage adding in roles later).

--- Main.java
+++ Main.java
@@ -8,67 +8,109 @@ Main file for handling register ID's, bank statments and transactions
 import java.util.Scanner;
 
 public class Main {
-
-    static int balance = 0;
-    static Scanner sc = new Scanner(System.in);
-
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         
-        curr_balance();
-        deposit();
-        withdraw();
-        curr_balance();
+        while (true) 
+        { 
+            System.out.print("""
+            1. Add Customer
+            2. Deposit
+            3. Withdraw
+            4. Check Balance
+            5. Exit
+            """);
+
+            int choice = sc.nextInt();
+    
+            switch(choice) {
+                case 1: customerDb(); break;
+                case 2: moneyManage(); break;
+            }
+        }
+        // moneyManage();
+        // customerDb();
     }
 
-    static void curr_balance()
+    public static void moneyManage()
+    {
+        BalanceManager bm = new BalanceManager();
+        bm.currBalance();
+        bm.depositBalance();
+        bm.withdrawBalance();
+        bm.currBalance();
+    }
+
+    public static void customerDb() 
+    {
+        Customer_DB cdb = new Customer_DB();
+        cdb.addCustomer();
+    }
+}
+
+class Customer_DB
+{
+    private Scanner sc = new Scanner(System.in);
+    private final String[] customer_db = new String[1000];
+    private int customerCount = 0;
+
+    void addCustomer()
+    {
+        System.out.println("What is your name?: "); 
+        String userName = sc.nextLine();
+        if ((userName != null) && (customerCount < customer_db.length))
+        {
+            customer_db[customerCount] = userName;
+            customerCount++;
+            System.out.println("Customer added: " + userName);
+
+        }
+        else
+        {
+            
+            System.out.println("Database is full!");
+            
+        }
+
+    }
+
+    // void removeCustomer()
+    // {
+
+    // }
+}
+
+
+class BalanceManager 
+{
+    private int balance = 0;
+    private Scanner sc = new Scanner(System.in);
+
+    void currBalance() 
     {
         System.out.println("Your current balance is: " + balance);
     }
 
-    static void deposit() 
+    void depositBalance() 
     {
         System.out.print("How much would you like to deposit?: ");
         int deposit = sc.nextInt();
-
         balance += deposit;
-
-        System.out.println("Your new balance is: " + balance);
     }
 
-    static void withdraw()
+    void withdrawBalance() 
     {
         System.out.print("How much would you like to withdraw?: ");
-        int withdraw = sc.nextInt();
-
-        balance -= withdraw;
-
-        System.out.println("Your new balance is: " + balance);
-    }
-
-    static void balance_change()
-    {
-
-        System.out.print("How much would you like to withdraw?: ");
-        System.out.print("How much would you like to deposit?: ");
+        int withdrawal = sc.nextInt();
+        if (withdrawal > balance)
+        {
+            System.out.print("You're amount exceeds your current balance\n");
+            withdrawBalance();
+        }
+        else
+        { 
+            balance -= withdrawal;
+        }
     }
 }
 
-// public class Customer_Database
-// {
-//     int customer_db[1000];
-
-//     static void CustomerAdd()
-//     {
-
-//     }
-
-//     static void CustomerRem()
-//     {
-
-//     }
-
-//     static void CustomerStatus()
-//     {
-
-//     }
-// }
